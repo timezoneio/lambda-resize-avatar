c9b6776537cacfcab88f6d0974ae1235219ad8a3
Dan Farrelly <daniel.j.farrelly@gmail.com>
2015-09-20 20:08:18 -0400

    Initial commit

---
+++ build.sh
@@ -0,0 +1,1 @@
+zip -r resizeAvatar.zip .
---
+++ index.js
@@ -0,0 +1,86 @@
+var async = require('async');
+var AWS = require('aws-sdk');
+var gm = require('gm').subClass({ imageMagick: true });
+var util = require('util');
+
+var MAX_WIDTH  = 200;
+var MAX_HEIGHT = 200;
+
+var s3 = new AWS.S3();
+
+exports.handler = function(event, context) {
+
+  var srcBucket = event.Records[0].s3.bucket.name;
+
+  // Remove spaces or non ASCII characers
+  var srcKey = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, ' '));
+
+  // NOTE - We're currently overwriting the original image
+  var dstBucket = srcBucket;
+  var dstKey = srcKey;
+
+  var typeMatch = srcKey.match(/\.([^.]*)$/);
+  if (!typeMatch) {
+    console.error('unable to infer image type for key ' + srcKey);
+    return;
+  }
+  var imageType = typeMatch[1];
+  if (imageType !== 'jpg' &&
+      imageType !== 'jpeg' &&
+      imageType !== 'png') {
+    console.log('skipping non-image ' + srcKey);
+    return;
+  }
+
+  async.waterfall([
+    function download(next) {
+      // Download the image from S3 into a buffer.
+      s3.getObject({
+        Bucket: srcBucket,
+        Key: srcKey
+      }, next);
+    },
+    function transform(response, next) {
+      gm(response.Body).size(function(err, size) {
+        // Infer the scaling factor to avoid stretching the image unnaturally.
+        var scalingFactor = Math.min(
+          MAX_WIDTH / size.width,
+          MAX_HEIGHT / size.height
+        );
+        var width  = scalingFactor * size.width;
+        var height = scalingFactor * size.height;
+
+        // Transform the image buffer in memory.
+        this.resize(width, height)
+          .toBuffer(imageType, function(err, buffer) {
+            if (err) return next(err);
+            next(null, response.ContentType, buffer);
+          });
+      });
+    },
+    function upload(contentType, data, next) {
+      // Stream the transformed image to a different S3 bucket.
+      s3.putObject({
+        Bucket: dstBucket,
+        Key: dstKey,
+        Body: data,
+        ContentType: contentType
+      }, next);
+    }
+  ], function (err) {
+    if (err) {
+      console.error(
+        'Unable to resize ' + srcBucket + '/' + srcKey +
+        ' and upload to ' + dstBucket + '/' + dstKey +
+        ' due to an error: ' + err
+      );
+    } else {
+      console.log(
+        'Successfully resized ' + srcBucket + '/' + srcKey +
+        ' and uploaded to ' + dstBucket + '/' + dstKey
+      );
+    }
+
+    context.done();
+  });
+};
